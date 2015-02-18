e04a4a56f954f58e8de2f56c8d0374e6401453ec
Jonathan W Young <jonyoung@Jonathans-MacBook-Pro.local>
2015-02-17 20:49:02 -0800

    Initial Commit

---
+++ ClassRoster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassRoster
+//
+//  Created by Jonathan W Young on 2/17/15.
+//  Copyright (c) 2015 Jonathan W Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
